25774107cc795623cdcb36ceb6b00ffb57ceffed
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-16 11:49:55 +0100

    learning Gin framework usin GORM

---
+++ middleware/auth.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"gotask/db"
+	"gotask/models"
+
+)
+
+func RequireAuth(c *gin.Context){
+	authHeader := c.GetHeader("Authorization")
+
+	if !strings.HasPrefix(authHeader, "Bearer "){
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+		return
+	}
+
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error){
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		return
+	}
+
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userID := uint(claims["sub"].(float64))
+
+	var user models.User
+
+	if err := db.DB.First(&user, userID).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "user not found"})
+		return
+	}
+
+	c.Set("currentUser", user)
+	c.Next()
+
+}
