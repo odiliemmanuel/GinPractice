6765cb782de6219862d2924d4db8afd7bec89a01
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-16 11:33:40 +0100

    learning Gin framework usin GORM

---
+++ handlers/auth_handler.go
@@ -0,0 +1,83 @@
+package handlers
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+
+	"gotask/db"
+	"gotask/models"
+)
+
+
+func RegisterUser(c*gin.Context){
+	var input models.RegisterInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not hash password"})
+		return
+	}
+
+	user := models.User{
+		Username: input.Username,
+		Email: input.Email, 
+		Password: string(hashedPassword),
+	}
+
+	if err := db.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "email already registered"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data" : gin.H{"id": user.ID, "username": user.Username, "email": user.Email}})
+}
+
+
+func LOginUser(c *gin.Context){
+	var input models.LoginInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user models.User
+
+	if err := db.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil{
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims {
+		"sub": user.ID,
+		"exp": time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	signed, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not create token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": signed})
+
+
+
+}
